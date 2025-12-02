94a4959aea2e457f331824f754c3723f9c1c2d76
syren-g <syren.galicia@egdcollective.org>
2025-12-02 15:12:32 -0500

    endofclass

--- Assets/Scripts/ObstableSpawner.cs
+++ Assets/Scripts/ObstableSpawner.cs
@@ -1,16 +1,52 @@
 using UnityEngine;
+using System.Collections.Generic;
 
-public class ObstableSpawner : MonoBehaviour
+public class EnemySpawner : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public List<GameObject> enemyPrefabs = new List<GameObject>();
+
+    public float timer = 0;
+    public float spawnRate = 5;
+
+    public float xMin = -8f;
+    public float xMax = 8f;
+    public float ySpawn = 6f;
+
     void Start()
     {
-        
+        timer = spawnRate;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            SpawnMultipleEnemies();
+            timer = spawnRate;
+
+
+        }
+
     }
+
+    public void SpawnRandomEnemy()
+    {
+        int randomI = Random.Range(0, enemyPrefabs.Count);
+        GameObject randomEnemy = enemyPrefabs[randomI];
+
+        Vector3 spawnPos = new Vector3(Random.Range(xMin, xMax), ySpawn, 0);
+        Instantiate(randomEnemy, spawnPos, Quaternion.identity);
+    }
+    public void SpawnMultipleEnemies()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            SpawnRandomEnemy();
+        }
+    }
+
 }
+
---
+++ Assets/Scripts/ObstacleMovment.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class HW7EnemyMove : MonoBehaviour
+{
+    public Vector3 moveDirection = new Vector3(0, -1, 0);
+    public float speed = 3;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += moveDirection * speed * Time.deltaTime;
+    }
+}
+
+
